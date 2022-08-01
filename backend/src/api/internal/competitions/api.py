9c159f923859e5534ef5cedccdee70fcb28b8229
Iuri Perov <iury.perov@gmail.com>
2022-08-01 14:04:22 +0500

    Added docker for dev

--- backend/src/api/internal/competitions/api.py
+++ backend/src/api/internal/competitions/api.py
@@ -20,7 +20,7 @@ from api.internal.users.domain.services import UserService
 def register_competitions_api(api: NinjaAPI) -> None:
     competition_service = CompetitionService(competition_repo, user_repo, field_repo)
     request_service = RequestService(request_repo, competition_repo, user_repo, participation_repo, form_value_repo)
-    field_service = FieldService(field_repo, default_repo)
+    field_service = FieldService(field_repo, default_repo, form_value_repo)
     user_service = UserService(user_repo, form_value_repo, request_repo, participation_repo)
 
     competition_handlers = CompetitionHandlers(competition_service, request_service, field_service, user_service)
