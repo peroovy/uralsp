32149566d6701f2a4ce9baf4c1b15bed4d18638e
Iuri Perov <iury.perov@gmail.com>
2022-08-05 12:43:29 +0500

    Moved serialization to utils

--- backend/src/api/internal/utils.py
+++ backend/src/api/internal/utils.py
@@ -1,5 +1,37 @@
-from typing import Dict, Optional
+import csv
+from io import BytesIO, StringIO
+from typing import Dict, Optional, Iterable, List
+
+from openpyxl.workbook import Workbook
+from openpyxl.worksheet.worksheet import Worksheet
 
 
 def get_strip_filters(**kwargs: Optional[str]) -> Dict[str, str]:
     return dict([*map(lambda p: [p[0], p[1].strip()], filter(lambda p: p[1] is not None, kwargs.items()))])
+
+
+def serialize_to_xlsx(rows: Iterable[Iterable[str]]) -> BytesIO:
+    buffer = BytesIO()
+    workbook = Workbook()
+    worksheet = workbook.active
+
+    for row, values in enumerate(rows, 1):
+        for column, value in enumerate(values, 1):
+            cell = worksheet.cell(row=row, column=column)
+            cell.value = value
+
+    workbook.save(buffer)
+    buffer.seek(0)
+
+    return buffer
+
+
+def serialize_to_csv(rows: Iterable[Iterable[str]], delimiter: str = ";") -> BytesIO:
+    buffer = StringIO()
+    writer = csv.writer(buffer, delimiter=delimiter)
+
+    for row in rows:
+        writer.writerow(row)
+
+    buffer.seek(0)
+    return BytesIO(buffer.read().encode("utf-8"))
