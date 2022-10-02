01e036b05b0f99a35807e89bc37a6d28e7b66341
Zeyad Alagamy <wezza2003@gmail.com>
2022-10-02 04:21:54 +0300

    solved nav bar

--- frontend/src/routes/+layout.ts
+++ frontend/src/routes/+layout.ts
@@ -2,8 +2,6 @@ import type { Load } from "@sveltejs/kit";
 
 export let load: Load = async function load({ fetch }) {
     // neglict the login route 
-    // wait for a second
-    await new Promise((resolve) => setTimeout(resolve, 1000));
 
     let API = import.meta.env.VITE_API_URL;
     let token_respond = await fetch(`${API}/auth/refresh`, {
@@ -13,7 +11,7 @@ export let load: Load = async function load({ fetch }) {
 
     if(!token_respond.ok && window.location.href != "https://reg.uralsp.ru/") {
         document.cookie = "sp_rt=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-        // window.location.href = 'https://reg.uralsp.ru/';
+        window.location.href = 'https://reg.uralsp.ru/';
     }
 
     let res = await token_respond.json();
